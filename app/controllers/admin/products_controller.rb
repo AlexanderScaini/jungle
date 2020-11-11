a805cc053a29fded03ffff7e0934ce8dc4fe871b
Alexander Scaini <alexanderscaini@gmail.com>
2020-11-11 22:42:30 +0000

    added basic auth to the admin pages

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['admin_name'], password: ENV['admin_password']
+
   def index
     @products = Product.order(id: :desc).all
   end
@@ -36,5 +38,4 @@ class Admin::ProductsController < ApplicationController
       :price
     )
   end
-
 end
