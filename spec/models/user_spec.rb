90200539366e71e019d40f8a69b7e40ef09717b6
Alexander Scaini <alexanderscaini@gmail.com>
2020-11-16 22:59:51 +0000

    finished the other specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,45 @@ RSpec.describe User, type: :model do
       expect(new_user.errors.full_messages).to include("Email has already been taken")
     end
 
+    it 'should not validate if the password is too short' do
+      new_user = User.new first_name: 'Bob', last_name: 'Burger', email: 'bob@gmail.com', password: '123', password_confirmation: '123'
+      new_user.save
+      expect(new_user.errors.full_messages).to include("Password is too short (minimum is 5 characters)")
+    end
+
+  end
+
+  describe '.authenticate_with_credentials' do
+    it 'should authenticate an email with the correct email and password' do
+      new_user = User.new first_name: 'Bob', last_name: 'Burger', email: 'bob@gmail.com', password: '12345', password_confirmation: '12345'
+      new_user.save
+      expect(new_user = User.authenticate_with_credentials('bob@gmail.com', '12345')).to_not be(nil)
+    end
+
+    it 'should not create a session if the email doesnt exist in the database' do
+      new_user = User.new first_name: 'Bob', last_name: 'Burger', email: 'bob@gmail.com', password: '12345', password_confirmation: '12345'
+      new_user.save
+      expect(new_user = User.authenticate_with_credentials('notbob@gmail.com', '12345')).to be(nil)
+    end
+
+    it 'should not create a session if the password doesnt match' do
+      new_user = User.new first_name: 'Bob', last_name: 'Burger', email: 'bob@gmail.com', password: '12345', password_confirmation: '12345'
+      new_user.save
+      expect(new_user = User.authenticate_with_credentials('bob@gmail.com', '54321')).to be(nil)
+    end
+
+    it 'should create a session if the email was input with spaces on either side of it' do
+      new_user = User.new first_name: 'Bob', last_name: 'Burger', email: 'bob@gmail.com', password: '12345', password_confirmation: '12345'
+      new_user.save
+      expect(new_user = User.authenticate_with_credentials('       bob@gmail.com    ', '12345')).to_not be(nil)
+    end
+
+    it 'should create a session if the email was input with a different case' do
+      new_user = User.new first_name: 'Bob', last_name: 'Burger', email: 'bob@gmail.com', password: '12345', password_confirmation: '12345'
+      new_user.save
+      expect(new_user = User.authenticate_with_credentials('BOB@GMAIL.com', '12345')).to_not be(nil)
+    end
+
   end
 
 end
